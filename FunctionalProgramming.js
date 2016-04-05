d19707f1bb5f841ee39fcdef8b25d163a5fb7857
Dennis Yakunchikov <Bisu@Denniss-MacBook-Pro.local>
2016-04-05 15:04:30 +0300

    updated monadEx
    more specific Monad usage example

--- FunctionalProgramming.js
+++ FunctionalProgramming.js
@@ -3,21 +3,46 @@
  */
 
 function monadEx() {
-    function MONAD() {
+    function MONAD( modifier ) {
 
-        return function unit( value ) {
+        var prototype = Object.create( null );
 
-            var monad = Object.create( null );
-            monad.bind = function( func ) {
-                return func( value );
+        function unit( value ) {
+
+            var monad = Object.create( prototype );
+            monad.bind = function( func, args ) {
+
+                return func.apply( undefined,
+                                   [ value ].concat(
+                                       Array.prototype.slice.apply( args || [] ) )
+                                 );
             };
+
+            if ( typeof modifier === "function" ) {
+                modifier( monad, value );
+            }
+
             return monad;
-        };
+        }
+
+        return unit;
     }
 
     var unit = MONAD();
 
     var monad = unit( "Hello" );
 
-    monad.bind( console.log );
+    monad.bind( console.log, [ 1, 2, 3 ] );
+
+    var maybe = MONAD( function( monad, value ) {
+        if ( value === null || value === undefined ) {
+            monad.isNull = true;
+            monad.bind = function() {
+                return monad;
+            };
+        }
+    } );
+
+    var monadMaybe = maybe( null );
+    monadMaybe.bind( null );
 }
