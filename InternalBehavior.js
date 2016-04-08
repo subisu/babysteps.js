5e744d9fcb7da8fe2b3ff87eeb1bab28d83b1284
Dennis Yakunchikov <Bisu@Denniss-MacBook-Pro.local>
2016-04-08 09:42:20 +0300

    why not to use "with" statement
    Example of:
    - changes to scope lookup chain
    - var statement within “with” block

--- InternalBehavior.js
+++ InternalBehavior.js
@@ -279,3 +279,50 @@ function funcInheritanceAndEnumeration() {
         }
     }
 }
+
+/**
+ * "with" statement behavior
+ * The with statement takes an object, one which has zero or more properties,
+ * and treats that object as if it is a wholly separate lexical scope, and thus
+ * the object's properties are treated as lexically defined identifiers in that "scope"
+ * Source: YDKJS
+ */
+function withBehavior() {
+
+    var objA = {
+        a: 1
+    };
+
+    var objB = {
+        b: 2
+    };
+
+    var b = 1;
+
+    function setObj( objToChange ) {
+
+        with ( objToChange ) {
+
+            // If "with" won't find b in object it will treat this variable
+            // as if there was no "with" statement. It will change variable
+            // b within parent function scope
+            b = 3;
+
+            // This won't declare new object property.
+            // New variable will be created within function scope
+            var c = 5;
+        }
+
+        // Even though a with block treats an object like a lexical scope,
+        // a normal var declaration inside that with block will not be scoped
+        // to that with block, but instead the containing function scope.
+        console.log(  "objToChange.c, c" );
+        console.log( objToChange.c + "      " + c );
+    }
+
+    setObj( objA );
+    setObj( objB );
+
+    console.log( "objA,     objB,   b" );
+    console.log( objA, objB, b );
+}
