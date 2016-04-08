05df079b28862ab56e304aba6fbae10c4252f397
Dennis Yakunchikov <Bisu@Denniss-MacBook-Pro.local>
2016-04-08 10:27:16 +0300

    more examples of scope behavior

--- Scope.js
+++ Scope.js
@@ -8,16 +8,26 @@
 function scopeUnusualBehavior() {
     "use strict";
 
-    for ( var i = 0; i < 10; i++ ) {
+    // Since there no var statement, i will represent
+    // parent function variable
+    function changer( ) {
+        i += 2;
+    }
+
+    for ( var i = 0; i < 10; i += 1 ) {
         console.log( i );
+
+        // This will change i
+        changer();
     }
 
     console.log( "out i", i );
 
-    for ( let i = 0; i < 5; i++ ) {
-        console.log( i );
+    for ( let i = 0; i < 5; i += 1 ) {
+        console.log( "Block i: ", i );
     }
 
     console.log( "out i", i );
 }
 
+scopeUnusualBehavior();
